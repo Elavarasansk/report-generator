bd817b28a46d913841b7743659fef99cb0121618
Elavarasansk <30699123+Elavarasansk@users.noreply.github.com>
2019-06-19 18:21:56 +0530

    Create MooduleConductConstants.java

---
+++ MooduleConductConstants.java
@@ -0,0 +1,19 @@
+package org.foryou.moodleconduct.constants;
+
+public class MooduleConductConstants {
+	
+	public static final int  INITIAL = 0 ; 
+	
+	public static final String QUESTION = "question" ; 
+	
+	public static final String QUESTION_BANK_NAME = "questionBankName" ; 
+	
+	public static final String VALUE = "value" ; 
+	
+	public static final String COUNT = "count" ; 
+
+
+
+
+
+}
